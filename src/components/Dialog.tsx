60798db604bf6494f03f59feb8fbc5334499d6af
mateen <mateenworld7861@gmail.com>
2020-10-20 20:44:08 +0500

    fn

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -49,6 +49,7 @@ const uiConfig = {
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: "/",
   // We will display Google and Facebook as auth providers.
+  
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
